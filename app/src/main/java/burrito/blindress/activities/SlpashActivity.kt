a6f64ec10ecbffa676af04d491911da4e62aa641
Ringov <by3dring@gmail.com>
2017-04-22 20:22:38 +0500

    creating first screen with button and playing audio on button click

--- app/src/main/java/burrito/blindress/activities/SlpashActivity.kt
+++ app/src/main/java/burrito/blindress/activities/SlpashActivity.kt
@@ -1,9 +1,11 @@
 package burrito.blindress.activities
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
+import android.view.Gravity
 import burrito.blindress.R
+import io.reactivex.Observable
 import org.jetbrains.anko.*
 
 /**
@@ -11,22 +13,45 @@ import org.jetbrains.anko.*
  */
 
 class SplashActivity : AppCompatActivity() {
+
+    lateinit var player: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         SplashUI().setContentView(this)
+        player = MediaPlayer.create(this, R.raw.hello)
     }
 }
 
 class SplashUI : AnkoComponent<SplashActivity> {
     override fun createView(ui: AnkoContext<SplashActivity>) = with(ui) {
         verticalLayout {
-            editText(R.string.hello_world) {
+            val btnStart = button(R.string.start_text) {
+                text = ui.ctx.getString(R.string.start_text)
                 textSize = 24f
-                hint = "Yoyo"
-                textChangedListener {
-                    onTextChanged { text, i1, i2, i3 -> Toast.makeText(ui.ctx, text, Toast.LENGTH_SHORT).show() }
+                horizontalGravity = Gravity.CENTER
+                verticalGravity = Gravity.CENTER
+                onClick {
+                    Observable.just(R.raw.hello, R.raw.i_need_your_cloths)
+                            //.delay(2000, TimeUnit.MILLISECONDS)
+                            .doOnNext {
+                                ui.owner.player = MediaPlayer.create(ui.ctx, it)
+                            }
+                            .flatMap {
+                                Observable.create<Boolean> { sub ->
+                                    ui.owner.player.start()
+                                    ui.owner.player.setOnCompletionListener {
+                                        it.release()
+                                        sub.onNext(true)
+                                        sub.onComplete()
+                                    }
+                                }
+                            }
+                            .subscribe()
+
                 }
-            }
+            }.lparams(width = matchParent,
+                    height = matchParent)
         }
     }
 
