04af7d5424ff5f5a58cfa873ec9c4a958fed40ff
Nikita Kraev <nightphoenix11@gmail.com>
2017-04-22 14:08:32 +0300

    Adding libraries

--- app/src/main/java/burrito/blindress/SlpashActivity.kt
+++ app/src/main/java/burrito/blindress/SlpashActivity.kt
@@ -1,16 +1,28 @@
 package burrito.blindress
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import org.jetbrains.anko.*
 
 /**
  * @author kitttn
  */
 
 class SplashActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.activity_splash)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        SplashUI().setContentView(this)
     }
+}
+
+class SplashUI : AnkoComponent<SplashActivity> {
+    override fun createView(ui: AnkoContext<SplashActivity>) = with(ui) {
+        verticalLayout {
+            editText(R.string.hello_world) {
+                textSize = 24f
+                hint = "Yoyo"
+            }
+        }
+    }
+
 }
