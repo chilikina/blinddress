152ce4daecb156ed488dac51eec3aa35f456222c
Nikita Kraev <nightphoenix11@gmail.com>
2017-04-22 16:13:54 +0300

    Created file structure

--- app/src/main/java/burrito/blindress/activities/SlpashActivity.kt
+++ app/src/main/java/burrito/blindress/activities/SlpashActivity.kt
@@ -1,7 +1,9 @@
-package burrito.blindress
+package burrito.blindress.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import burrito.blindress.R
 import org.jetbrains.anko.*
 
 /**
@@ -21,8 +23,12 @@ class SplashUI : AnkoComponent<SplashActivity> {
             editText(R.string.hello_world) {
                 textSize = 24f
                 hint = "Yoyo"
+                textChangedListener {
+                    onTextChanged { text, i1, i2, i3 -> Toast.makeText(ui.ctx, text, Toast.LENGTH_SHORT).show() }
+                }
             }
         }
     }
 
+    // 10.240.18.96: 8000
 }
